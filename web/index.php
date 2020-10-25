25d869e74679b32495fd54cc837333b13340886a
unknown <jamefo_90@hotmail.com>
2020-10-25 10:30:33 -0500

    php retorno

--- web/index.php
+++ web/index.php
@@ -2,8 +2,6 @@
 
 require('../vendor/autoload.php');
 
-use Symfony\Component\HttpFoundation\Request;
-
 $app = new Silex\Application();
 $app['debug'] = true;
 
@@ -22,31 +20,6 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
-});
-
-
-$app->get('/ruta', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-//Ruta de demostración, se recibe(n) dato(s) y se manipulan
-$app->post('/Datoenviado', function (Request $request) use ($app) {
-   	
-
-    
-$dbconn = pg_pconnect("host=ec2-35-169-92-231.compute-1.amazonaws.com port=5432 dbname=d40d9mehlild8g user=wsslccaolqixxt password=7809ae03fd8da52449097500903b66b89591dfa44e9fecfb9100605a0eb7b1c1");
-
-	if($dbconn){
-	return"me conecte"
-		}
-	else{
-		return"Conexion Fallida"
-	}
-
-   	$nombre = $request->get('nombre');
-	$respuesta = "Esp32ok" .$nombre;
-   	return $respuesta;
 });
 
 $app->run();
