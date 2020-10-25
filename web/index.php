a31d1aac70a2603d8ee994b9a5c21f999cd13f30
unknown <jamefo_90@hotmail.com>
2020-10-25 15:28:57 -0500

    bd

--- web/index.php
+++ web/index.php
@@ -25,14 +25,14 @@ $app->get('/', function() use($app) {
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
 $app->post('/guardardato', function (Request $request) use ($app) {
    
-   $corriente = $request->get('corriente');
+   $motorsense = $request->get('motorsense');
   	
 
 $dbconn = pg_pconnect("host=ec2-35-169-92-231.compute-1.amazonaws.com port=5432 dbname=d40d9mehlild8g user=wsslccaolqixxt password=7809ae03fd8da52449097500903b66b89591dfa44e9fecfb9100605a0eb7b1c1");
 
 $data= array(
 		"fecha"=>date('Y-m-d H:i:s'),
-		"corriente"=> $corriente
+		"motorsense"=> $motorsense
 		
 		
 );
