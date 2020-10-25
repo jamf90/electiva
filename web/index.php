2b1e98d7c874ce06fc9786086f2217f15d21dbd8
unknown <jamefo_90@hotmail.com>
2020-10-25 11:22:05 -0500

    php8

--- web/index.php
+++ web/index.php
@@ -1,9 +1,12 @@
 <?php
 
 require('../vendor/autoload.php');
+
 use Symfony\Component\HttpFoundation\Request;
+
 $app = new Silex\Application();
 $app['debug'] = true;
+
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
@@ -23,19 +26,10 @@ $app->get('/ruta', function() use($app) {
 });
 
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
-$app->post('/Datoenviado', function (Request $request) use ($app) {
-
-$dbconn = pg_pconnect("host=ec2-35-169-92-231.compute-1.amazonaws.com port=5432 dbname=d40d9mehlild8g user=wsslccaolqixxt password=7809ae03fd8da52449097500903b66b89591dfa44e9fecfb9100605a0eb7b1c1");
-
-  if($dbconn){
-  return"me conecte"
-    }
-  else{
-    return"Conexion Fallida"
-  }
-
+$app->post('/modificarDato', function (Request $request) use ($app) {
     $nombre = $request->get('nombre');
-  $respuesta = "Esp32ok" .$nombre;
+  $respuesta = "Hola " .$nombre;
     return $respuesta;
 });
+
 $app->run();
