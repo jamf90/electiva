ed80be5b29bc52928beb844adf6221e0f9f7cf6c
unknown <jamefo_90@hotmail.com>
2020-10-25 12:57:22 -0500

    php14

--- web/index.php
+++ web/index.php
@@ -25,7 +25,7 @@ $app->get('/', function() use($app) {
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
 $app->post('/guardardato', function (Request $request) use ($app) {
    
-$dbconn = pg_pconnect("host=ec2-35-169-92-231.compute-1.amazonaws.comm port=5432 dbname=d40d9mehlild8g user=wsslccaolqixxt password=7809ae03fd8da52449097500903b66b89591dfa44e9fecfb9100605a0eb7b1c1");
+$dbconn = pg_pconnect("host=ec2-35-169-92-231.compute-1.amazonaws.com port=5432 dbname=d40d9mehlild8g user=wsslccaolqixxt password=7809ae03fd8da52449097500903b66b89591dfa44e9fecfb9100605a0eb7b1c1");
 
 if ($dbconn){
 	return "Me conecté";
