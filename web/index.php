47f69cd9594403758065fbee05b617825a2b7862
unknown <jamefo_90@hotmail.com>
2020-10-25 10:56:29 -0500

    php5

--- web/index.php
+++ web/index.php
@@ -2,6 +2,8 @@
 
 require('../vendor/autoload.php');
 
+use Symfony\Component\HttpFoundation\Request;
+
 $app = new Silex\Application();
 $app['debug'] = true;
 
@@ -9,17 +11,25 @@ $app['debug'] = true;
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
-
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
-
 // Our web handlers
-
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
+});
+$app->get('/ruta', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return $app['twig']->render('index.twig');
+});
+
+//Ruta de demostración, se recibe(n) dato(s) y se manipulan
+$app->post('/modificarDato', function (Request $request) use ($app) {
+    $nombre = $request->get('nombre');
+  $respuesta = "Hola " .$nombre;
+    return $respuesta;
 });
 
 $app->run();
