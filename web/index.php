707de19b7950af17d96690edeb95e6aaffc562c7
unknown <jamefo_90@hotmail.com>
2020-10-25 12:18:45 -0500

    php11

--- web/index.php
+++ web/index.php
@@ -23,7 +23,19 @@ $app->get('/', function() use($app) {
 
 
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
-$app->post('/modificarDato', function (Request $request) use ($app) {
+$app->post('/guardardato', function (Request $request) use ($app) {
+   
+$dbconn = pg_pconnect("host=ec2-35-169-92-231.compute-1.amazonaws.com port=5432 dbname=d40d9mehlild8g user=wsslccaolqixxt password=7809ae03fd8da52449097500903b66b89591dfa44e9fecfb9100605a0eb7b1c1")
+
+if ($dbconn){
+	return "Me conecté";
+}
+else{
+	return"Conexion fallida";
+}
+
+
+
     $nombre = $request->get('nombre');
   $respuesta = "Hola " .$nombre;
     return $respuesta;
