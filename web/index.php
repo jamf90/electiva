167635fac551f0572d1a9b18d2f7ac12ab15bad1
unknown <jamefo_90@hotmail.com>
2020-10-25 11:02:41 -0500

    php6

--- web/index.php
+++ web/index.php
@@ -1,12 +1,11 @@
 <?php
 
-require('../vendor/autoload.php');
 
 use Symfony\Component\HttpFoundation\Request;
+require('../vendor/autoload.php');
 
 $app = new Silex\Application();
 $app['debug'] = true;
-
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
@@ -20,15 +19,23 @@ $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
 });
-$app->get('/ruta', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
 
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
-$app->post('/modificarDato', function (Request $request) use ($app) {
+$app->post('/Datoenviado', function (Request $request) use ($app) {
+    
+
+    
+$dbconn = pg_pconnect("host=ec2-35-169-92-231.compute-1.amazonaws.com port=5432 dbname=d40d9mehlild8g user=wsslccaolqixxt password=7809ae03fd8da52449097500903b66b89591dfa44e9fecfb9100605a0eb7b1c1");
+
+  if($dbconn){
+  return"me conecte"
+    }
+  else{
+    return"Conexion Fallida"
+  }
+
     $nombre = $request->get('nombre');
-  $respuesta = "Hola " .$nombre;
+  $respuesta = "Esp32ok" .$nombre;
     return $respuesta;
 });
 
