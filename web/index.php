48080df5bbd842ef7aa3b29b4f36522175ad99d6
unknown <jamefo_90@hotmail.com>
2020-10-25 13:20:01 -0500

    php15

--- web/index.php
+++ web/index.php
@@ -27,13 +27,12 @@ $app->post('/guardardato', function (Request $request) use ($app) {
    
 $dbconn = pg_pconnect("host=ec2-35-169-92-231.compute-1.amazonaws.com port=5432 dbname=d40d9mehlild8g user=wsslccaolqixxt password=7809ae03fd8da52449097500903b66b89591dfa44e9fecfb9100605a0eb7b1c1");
 
-if ($dbconn){
-	return "Me conecté";
-}
-else{
-	return"Conexion fallida";
-}
-
+$data= array(
+		"fecha"=>date('Y-m-d H:i:s'),
+		"voltaje"=> $voltaje
+);
+$respuesta = pg_insert($dbconn, "Motor_view", $data);
+return $respuesta;
 
   $nombre = $request->get('nombre');
   $respuesta = "Hola " .$nombre;
