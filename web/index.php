8f683470860f98d66426a68feab50db0a73dbe88
unknown <jamefo_90@hotmail.com>
2020-10-25 16:21:39 -0500

    db9

--- web/index.php
+++ web/index.php
@@ -26,6 +26,7 @@ $app->get('/', function() use($app) {
 $app->post('/guardardato', function (Request $request) use ($app) {
    
    $motorsense = $request->get('motorsense');
+   $voltaje = $request->get('voltaje');
   	
 
 $dbconn = pg_pconnect("host=ec2-35-169-92-231.compute-1.amazonaws.com port=5432 dbname=d40d9mehlild8g user=wsslccaolqixxt password=7809ae03fd8da52449097500903b66b89591dfa44e9fecfb9100605a0eb7b1c1");
@@ -33,7 +34,7 @@ $dbconn = pg_pconnect("host=ec2-35-169-92-231.compute-1.amazonaws.com port=5432 
 $data= array(
 		"fecha"=>date('Y-m-d H:i:s'),
 		"motorsense"=> $motorsense
-		
+		"voltaje"=> $voltaje
 		
 );
 $respuesta = pg_insert($dbconn, "Motor_view", $data);
