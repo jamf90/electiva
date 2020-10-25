5922804c54d2298ab20f967b357acc35b71bfc61
unknown <jamefo_90@hotmail.com>
2020-10-25 11:43:23 -0500

    php9

--- web/index.php
+++ web/index.php
@@ -20,16 +20,32 @@ $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
 });
-$app->get('/ruta', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+
+//Ruta de demostración, para validar que se recibe(n) dato(s) y se responde con este mismo
+$app->post('/enviarDato', function (Request $request) use ($app) {
+   return $request;
 });
+
 
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
 $app->post('/modificarDato', function (Request $request) use ($app) {
+$app->post('/guardarDato', function (Request $request) use ($app) {
+
+  $dbconn = pg_pconnect("host=ec2-52-21-0-111.compute-1.amazonaws.com port=5432 dbname=da23ojrg1de3ae user=msmhlrvxhgltyv password=baf2024024b59cdd7b5bd1a44e8d8a7773810a5ccbce3719f01225c9baac9bf2");
+
+  if($dbconn){
+    return "Me conecté"
+  }
+  else{
+    return "Conexión Fallida"
+  }
+
     $nombre = $request->get('nombre');
   $respuesta = "Hola " .$nombre;
     return $respuesta;
 });
-
+//Ruta de demostración, se recibe(n) dato(s) y se manipulan
+$app->post('/postArduino', function (Request $request) use ($app) {
+    return "OK";
+});
 $app->run();
