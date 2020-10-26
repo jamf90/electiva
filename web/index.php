f5817779c47ac6461044e793b93f65e91fed7381
unknown <jamefo_90@hotmail.com>
2020-10-26 03:52:56 -0500

    consulta

--- web/index.php
+++ web/index.php
@@ -48,11 +48,15 @@ return $respuesta;
 
 });
 
+//consulta dato 
+
 $app->get('/consultardato', function (Request $request) use ($app) {
 
 $dbconn = pg_pconnect("host=ec2-35-169-92-231.compute-1.amazonaws.com port=5432 dbname=d40d9mehlild8g user=wsslccaolqixxt password=7809ae03fd8da52449097500903b66b89591dfa44e9fecfb9100605a0eb7b1c1");
 
 $consulta=pg_query($dbconn,"SELECT * FROM Motor_view" );
+
+print_r(pg_fetch_all($consulta));
 return pg_fetch_all($consulta);
 
 
