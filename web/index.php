0a789908ea85d55d98ff88b0adaddc570a81cffb
jamf90 <70180507+jamf90@users.noreply.github.com>
2020-10-30 21:56:30 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -45,7 +45,7 @@ $data= array(
 );
 //$respuesta = pg_insert($dbconn, $tabla, $data);
 	
-$query = "INSERT INTO " . $tabla . "(fecha,voltaje,motor,corriente,) VALUES ('" . date('Y-m-d H:i:s') . "'," . $voltaje . ",'" .$motor   ."', " . $corriente . ");" ;
+$query = "INSERT INTO " . $tabla . "(fecha,voltaje,motor,corriente,) VALUES ('" . date('Y-m-d H:i:s') . "'," . $voltaje . ",'" .$motor   ."', " . $corriente . ")" ;
 	$respuesta = pg_query($dbconn, $query);
 
 	echo $query; echo "<br><br>";
