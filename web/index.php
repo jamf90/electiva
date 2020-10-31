19fca5b2b726445bb45ee3e9feec07b78840a2e2
jamf90 <70180507+jamf90@users.noreply.github.com>
2020-10-30 21:42:11 -0500

    Update index.php

--- web/index.php
+++ web/index.php
@@ -27,8 +27,8 @@ $app->post('/guardardato', function (Request $request) use ($app) {
    
    $voltaje = $request->get('voltaje');
    $tabla = $request->get('tabla');
-
-
+   $motorsense=$request->get('motor');
+   $corriente =$request->get('corriente');
   
   	
 
@@ -38,8 +38,8 @@ $data= array(
 		"fecha"=>date('Y-m-d H:i:s'),
 		
 		"voltaje"=> $voltaje,
-		"motorsense"=>$request->get('motor'),
-		"corriente" => $request->get('corriente')
+		"motor"=> $motorsense
+		"corriente" =>$corriente 
 		
 		
 );
